060b84be7d8f9d38198145661e5b7b92fae445d8
Cordelia-T <cctrueax@willamette.edu>
2024-09-23 19:23:51 -0700

    Submitting Milestone 0

--- Wordle.py
+++ Wordle.py
@@ -1,5 +1,5 @@
 ########################################
-# Name:
+# Name: Cordelia Trueax
 # Collaborators (if any):
 # GenAI Transcript (if any):
 # Estimated time spent (hr):
@@ -15,11 +15,28 @@ def wordle():
 
     def enter_action():
         # What should happen when RETURN/ENTER is pressed.
-        gw.show_message("You need to implement this method")
+        # gw.show_message("You need to implement this method")
+        word_to_row("Sofie", 0)
+        print(word_from_row(0))
 
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
+
+
+    # prints letters from the word into the wordle grid one at a time 
+    # input : 5 letter word
+    def word_to_row(word : str, row : int):
+        for i in range(len(word)):
+            gw.set_square_letter(row, i, word[i])
+ 
+    def word_from_row(row : int) -> str:
+        temp = ''
+
+        for i in range(5):
+            temp+= gw.get_square_letter(row, i)
+
+        return temp
 
 
 
@@ -27,3 +44,7 @@ def wordle():
 # Startup boilerplate
 if __name__ == "__main__":
     wordle()
+
+
+
+
