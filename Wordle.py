7552c078042872978d209e1eb283e5bf8689666a
Cordelia-T <cctrueax@willamette.edu>
2024-09-29 12:03:17 -0700

    Submitting Milestone 5 and bug fix for Milestone 3

--- Wordle.py
+++ Wordle.py
@@ -1,9 +1,9 @@
 ########################################
 # Name: Cordelia Trueax
-# Collaborators (if any):
-# GenAI Transcript (if any):
-# Estimated time spent (hr):
-# Description of any added extensions:
+# Collaborators (if any): N/A (Aside from some discussion with Sophie)
+# GenAI Transcript (if any): N/A
+# Estimated time spent (hr):3-4
+# Description of any added extensions: N/A
 ########################################
 
 from WordleGraphics import *  # WordleGWindow, N_ROWS, N_COLS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR
@@ -15,25 +15,20 @@ def wordle():
 
     def enter_action():
         # What should happen when RETURN/ENTER is pressed.
-        
-        # word_to_row("Laugh", 0)
-        # print(word_from_row(0))
 
     #    Keeps track of what the current row is
         curr_row = gw.get_current_row()
-        print(curr_row)
-
-        N_ROW = 5
-
-        # # game ends if curr_row is beyond the last row (#5)
-        # if curr_row>5:
-           
-        
-        # but if game is not over...
-        print("hi")
 
            # reads input on current line
         input = word_from_row(curr_row)
+
+
+        # finds the letters which are the same in the input and to_guess
+        for i in range(len(input)):
+            if input[i].lower() == to_guess[i]:
+                correct_letters.append(input[i])
+        
+        print(correct_letters)
         
                     # if the guess is an invalid word, displays according message
         if not valid_word(input):
@@ -50,7 +45,7 @@ def wordle():
                 # if the game is not yet won, moves on to the next row
             else:
                     # if it's on the last row, the player has run out of turns
-                if curr_row == N_ROW:
+                if curr_row == N_ROWS:
                     gw.show_message("Out of Guesses 👎 The answer was: " + to_guess)
                     
                 # otherwise it increments the row
@@ -59,7 +54,7 @@ def wordle():
 
         
 
-
+# =============================== Pre-Game Setup ====================================
 
     
     # picks a random word and assigns it to the to_guess variable
@@ -74,23 +69,37 @@ def wordle():
             temp = random.choice(ENGLISH_WORDS)
         
         return temp
+    
+
+    
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
     
-    # Globals
+    # Sets the to_guess word before the game begins
     # to_guess = set_to_guess()
     # to_guess = "glass"
     to_guess = set_to_guess()
     print(to_guess)
-    # to_guess = "lapsl"
 
+    correct_letters = []
+
+
+    # ===============================================================================
+
+
+
+
+# ================================ Gameplay Functions ================================
 
     # prints letters from the word into the wordle grid one at a time 
     # input : 5 letter word
     def word_to_row(word : str, row : int):
         for i in range(len(word)):
             gw.set_square_letter(row, i, word[i])
+
+
+
  
     # returns the word currently printed in a given row
     def word_from_row(row : int) -> str:
@@ -100,9 +109,8 @@ def wordle():
             temp+= gw.get_square_letter(row, i)
 
         return temp
-    
-    # # defines the word that the player will guess
-    # def set_to_guess():
+
+
 
     
     # given an input word, checks if word aligns with to_guess word
@@ -137,75 +145,67 @@ def wordle():
         # loops through letters in the guessed word
         for i in range(len(guess)):
             # counts up how many times the letter has appeared in the word, up to and including the current point 
-            # (if letters are repeated, the last time around will be when it correcly colors everything since it counts up letters before occurence)
-            # copies = [0,0,0,0,0]
 
+            # determines which instance the current letter is out of the same letter in the word (e.g. 1st S, 2nd S, 3rd S...)
+            # Is passed into check_letter to determine how many of each letter has been colored yellow
             for j in range(0, i+1):
                 # convert to lower case so it may be properly compared
                 curr_letter = guess[j].lower()
-
-                # print(curr_letter, to_guess[j])
-                # if curr_letter == to_guess[j]:
-                #     copies[j] = copies[j] + 1
-                    
-
-                # curr_letter == guess[i] and not
-                # print(copies)
-                
                 
                 copies = 0
                 if curr_letter == to_guess[j]:
-                    copies -=1
-                    # print(copies)
-
+                    copies +=1
                 
 
-
-            # subtracts from the potential copies if one of them is correctly placed - fixes the case where letters turn yellow without accounting for green further down the word
-            # for j in to_guess:
-            #     if guess[i] == :
-            #         copies-=1
-
-            # loop through both words 
-            # if two of them have the same current letter in the same place, 
-            # subtract 1 from copies
-
-            # goes through the colors returned from check_letter, and colors the square
+            # goes through the colors returned from check_letter, and colors the squares and keys
             match (check_letter(guess[i].lower(), i, copies, guess)):
+            # match(check_letter(guess[i].lower(), i, guess)):
                 case 'green':
                     gw.set_square_color(row, i, CORRECT_COLOR)
+                    gw.set_key_color(guess[i], CORRECT_COLOR)
                 
                 case 'yellow':
                     gw.set_square_color(row, i, PRESENT_COLOR)
+                    # doesn't recolor key if the letter has already been correctly guessed
+                    if not guess[i] in correct_letters:
+                        gw.set_key_color(guess[i], PRESENT_COLOR)
 
                 case 'grey': 
                     gw.set_square_color(row, i, MISSING_COLOR)
+                    gw.set_key_color(guess[i], MISSING_COLOR)
 
                 case _:
                     gw.set_square_color(row, i, UNKNOWN_COLOR)
 
 
+
+
+
+
     # checks if the letter is in the word/ if its in the right spot - returns to color_letters function
     # guess_copies parameter is how many times the letter has shown up in the word before (and including) the current index
     def check_letter(letter: str, idx: int, guess_copy: int, guess: str) -> str:
+    # def check_letter(letter: str, idx: int, guess: str) -> str:
+
         # converts everything to lower case
         letter = letter.lower()
         guess = guess.lower()
 
-        # initializes the number of times the letter shows up in to_guess
+        # initializes variable to hold the number of times the letter shows up in to_guess
         to_guess_copies = 0
 
-        for i in range(len(to_guess)):
-            # print(to_guess[i]== guess[i])
-            # print(to_guess[i], guess[i])
-            # print(letter)
-            if to_guess[i] == letter and not to_guess[i]==guess[i]:
-                to_guess_copies+=1
-            # print(to_guess_copies)
 
-
-        # print(to_guess_copies)
-
+        # variable to hold the number of times a letter shows up in the word
+        copies = 0
+        # counts up how many times the letter appears
+        for i in to_guess:
+            if i == letter:
+                to_guess_copies += 1
+                
+            # subtracts any instances where the correct placement has already been guessed
+            for j in range(len(correct_letters)):
+                if correct_letters[j].lower() == letter and j == idx:
+                    to_guess_copies -= 1
 
         # if letter is at the current index, color green
         if to_guess[idx] == letter:
@@ -220,11 +220,7 @@ def wordle():
             return "grey"
         
 
-
-
-
-
-
+#  ================================================================================
 
 
 
